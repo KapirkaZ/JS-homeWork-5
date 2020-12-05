6f2197f2b98dc86ff5393f5c61d6aebc6777ba22
Kapirka <68941822+KapirkaZ@users.noreply.github.com>
2020-12-05 16:07:35 +0200

    Задача 5-3
    использование методов класса

---
+++ 3-task.js
@@ -0,0 +1,60 @@
+ // Write code under this line
+ class Storage {
+    constructor(items) {
+      this.items = items;
+    }
+  
+    getItems() {
+      return this.items;
+    }
+  
+    addItem(item) {
+      return this.items.push(item);
+    }
+  
+    removeItem(item) {
+      let index = this.items.indexOf(item);
+      if (index > -1) {
+        return this.items.splice(index, 1);
+      }
+    }
+  }
+  
+   console.log(typeof Storage);
+  // 'function'
+  
+  const goods = [
+    'Нанитоиды',
+    'Пролонгер',
+    'Железные жупи',
+    'Антигравитатор'
+  ];
+  
+   const storage = new Storage(goods);
+  
+   console.log(storage.getItems());
+  /* [
+    'Нанитоиды',
+    'Пролонгер',
+    'Железные жупи',
+    'Антигравитатор'
+  ] */
+  
+   storage.addItem('Дроид');
+   console.log(storage.getItems());
+  /* [
+    'Нанитоиды',
+    'Пролонгер',
+    'Железные жупи',
+    'Антигравитатор',
+    'Дроид'
+  ] */
+  
+   storage.removeItem('Пролонгер');
+   console.log(storage.getItems());
+  /* [
+    'Нанитоиды',
+    'Железные жупи',
+    'Антигравитатор',
+    'Дроид'
+  ] */
